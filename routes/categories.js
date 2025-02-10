1f10d27d73acf756e813274bf8f14b4e48c2aeed
melbin2 <melbinbennyoffl@gmail.com>
2025-02-10 17:18:56 +0530

    bug fix

--- routes/categories.js
+++ routes/categories.js
@@ -1,24 +1,55 @@
+
 const express = require('express');
-const { ObjectId } = require('mongodb');
+const Category = require('../models/category');
 const router = express.Router();
-const db  = require('../utils/mongoDBConnection');
 
-
-// Fetch all categories
 router.get('/', async (req, res) => {
     try {
-        const categories = await db.get().collection('categories').find().toArray();
+        const categories = await Category.find({});
+        res.json(categories);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+router.get('/:id', async (req, res) => {
+    try {
+        const categories = await Category.findOne({_id:req.params.id});
         res.json(categories);
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
 });
 
-// Fetch category by ID
-router.get('/:id', async (req, res) => {
+router.post('/', async (req, res) => {
     try {
-      
-        const category = await  db.get().collection('categories').findOne({ _id:new ObjectId(req.params.id) });
+        console.log(req.body)
+        const category = new Category(req.body);
+        await category.save();
+        res.status(201).json(category);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const category = await Category.findByIdAndDelete(req.params.id);
+        if (!category) {
+            return res.status(404).json({ message: 'Category not found' });
+        }
+        res.json({ message: 'Category removed', category });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    try {
+        const category = await Category.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true, runValidators: true }
+        );
         if (!category) {
             return res.status(404).json({ message: 'Category not found' });
         }
@@ -28,135 +59,80 @@ router.get('/:id', async (req, res) => {
     }
 });
 
-// Create a new category
-router.post('/', async (req, res) => {
-    try {
-        
-        const result = await  db.get().collection('categories').insertOne(req.body);
-        res.status(201).json({message: "creatred succeffully"});  // Return the newly created category
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-});
 
-// Delete category by ID
-router.delete('/:id', async (req, res) => {
-    try {
-        
-        const result = await  db.get().collection('categories').deleteOne({ _id:new ObjectId(req.params.id) });
-        if (result.deletedCount === 0) {
-            return res.status(404).json({ message: 'Category not found' });
-        }
-        res.json({ message: 'Category removed' });
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-});
-
-// Update category by ID
-router.put('/:id', async (req, res) => {
-    try {
-        
-        const result = await  db.get().collection('categories').updateOne(
-            { _id:new ObjectId(req.params.id) },
-            { $set: req.body }
-        );
-        if (result.matchedCount === 0) {
-            return res.status(404).json({ message: 'Category not found' });
-        }
-        const updatedCategory = await db.collection('categories').findOne({ _id:new ObjectId(req.params.id) });
-        res.json(updatedCategory);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-});
-
-// Add an item to a category
 router.post('/:categoryId/items', async (req, res) => {
     try {
-       
-        const category = await  db.get().collection('categories').findOne({ _id:new ObjectId(req.params.categoryId) });
+        const category = await Category.findById(req.params.categoryId);
         if (!category) {
             return res.status(404).json({ message: 'Category not found' });
         }
-        const updatedCategory = await db.get().collection('categories').updateOne(
-            { _id:new ObjectId(req.params.categoryId) },
-            { $push: { items: req.body } }
-        );
-        res.status(201).json(updatedCategory);
+        category.items.push(req.body);
+        await category.save();
+        res.status(201).json(category);
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
 });
-
-// Get all items from a category
 router.get('/:categoryId/items', async (req, res) => {
     try {
-       
-        const category = await  db.get().collection('categories').findOne({ _id:new ObjectId(req.params.categoryId) });
+        const category = await Category.findById(req.params.categoryId);
         if (!category) {
             return res.status(404).json({ message: 'Category not found' });
         }
-        res.json(category.items);
+      
+        res.status(200).json(category);
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
 });
 
-// Delete an item from a category
 router.delete('/:categoryId/items/:itemId', async (req, res) => {
     try {
-        
-        const category = await  db.get().collection('categories').findOne({ _id:new ObjectId(req.params.categoryId) });
+        const category = await Category.findById(req.params.categoryId);
         if (!category) {
             return res.status(404).json({ message: 'Category not found' });
         }
-        const updatedCategory = await db.get().collection('categories').updateOne(
-            { _id:new ObjectId(req.params.categoryId) },
-            { $pull: { items: { _id:new ObjectId(req.params.itemId) } } }
+       
+        category.items = category.items.filter(
+            (item) => item.id.toString() !== req.params.itemId
         );
-        res.json({ message: 'Item removed' });
+        await category.save();
+        res.json({ message: 'Item removed', category });
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
 });
 
-// Update an item in a category
 router.put('/:categoryId/items/:itemId', async (req, res) => {
     try {
-        
-        const category = await  db.get().collection('categories').findOne({ _id:new ObjectId(req.params.categoryId) });
+        const category = await Category.findById(req.params.categoryId);
         if (!category) {
             return res.status(404).json({ message: 'Category not found' });
         }
-
-        const updatedCategory = await db.get().collection('categories').updateOne(
-            { _id:new ObjectId(req.params.categoryId), 'items._id':new ObjectId(req.params.itemId) },
-            { $set: { 'items.$': req.body } }
+        const item = category.items.find(
+            (item) => item.id.toString() === req.params.itemId
         );
-
-        if (updatedCategory.matchedCount === 0) {
-            return res.status(404).json({ message: 'Item not found' });
-        }
-
-        res.json({ message: 'Item updated' });
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-});
-
-// Get a specific item in a category
-router.get('/:categoryId/items/:itemId', async (req, res) => {
-    try {
-      
-        const category = await  db.get().collection('categories').findOne({ _id:new ObjectId(req.params.categoryId) });
-        if (!category) {
-            return res.status(404).json({ message: 'Category not found' });
-        }
-        const item = category.items.find(item => item._id.toString() === req.params.itemId);
         if (!item) {
             return res.status(404).json({ message: 'Item not found' });
         }
+        item.set(req.body);
+        await category.save();
+        res.json(category);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+router.get('/:categoryId/items/:itemId', async (req, res) => {
+    try {
+        const category = await Category.findById(req.params.categoryId);
+        if (!category) {
+            return res.status(404).json({ message: 'Category not found' });
+        }
+        const item = category.items.find(
+            (item) => item.id.toString() === req.params.itemId
+        );
+        
         res.json(item);
     } catch (err) {
         res.status(500).json({ message: err.message });
--- utils/mongoDBConnection.js
+++ utils/mongoDBConnection.js
@@ -1,25 +1,32 @@
 const { MongoClient } = require('mongodb');
+const mongoose = require('mongoose');
 
-  const mongoURI = 'mongodb+srv://melbinbennyoffl:melbin@cluster0.xsit3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-
-const state = {}
-
-const dbName = process.env.DATABASE_NAME || 'sungglebugs'
-const client = new MongoClient(mongoURI)
-module.exports.connect = async function (done) {
-  // Use connect method to connect to the server
-  await client.connect((err,data)=>{
-      
-      if(err) return done(err)
-      state.db=client.db(dbName)
-     
-
-  })
-  state.db=client.db(dbName)
-  done()
-
-}
-
-module.exports.get= function (){
-  return state.db
-}
+const mongoURI = 'mongodb+srv://melbinbennyoffl:melbin@cluster0.xsit3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+  
+  const state = {}
+  
+  const dbName = process.env.DATABASE_NAME || 'ventureHub'
+  const client = new MongoClient(mongoURI)
+  module.exports.connect = async function (done) {
+   
+    await client.connect((err,data)=>{
+        
+        if(err) return done(err)
+        state.db=client.db(dbName)
+       
+  
+    })
+    state.db=client.db(dbName)
+    done()
+  
+  }
+  
+  
+  mongoose.connect(mongoURI,{dbName})
+      .then(() => {
+          console.log("Connected Established!")
+      })
+  
+  module.exports.get= function (){
+    return state.db
+  }
